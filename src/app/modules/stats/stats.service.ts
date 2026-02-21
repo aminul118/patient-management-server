4cbacb39de113dc63d5e2933d6c5598c4dab9820
MD AMINUL ISLAM <mr.aminul118@gmail.com>
2026-02-21 20:24:13 +0600

    Admin stats fix

--- src/app/modules/stats/stats.service.ts
+++ src/app/modules/stats/stats.service.ts
@@ -1,16 +1,35 @@
 import { Gdm } from '../patient-management/gdm/gdm.model';
 import { User } from '../user/user.model';
+import { Infertility } from '../patient-management/infertility/infertility.model';
+import { OverWeight } from '../patient-management/over-weight/over-weight.model';
+import { Pcos } from '../patient-management/pcos/pcos.model';
 
 const getAdminStats = async () => {
-  const [userCount, GdmPatientCount] = await Promise.all([
+  const [
+    userCount,
+    GdmPatientCount,
+    InfertilityPatientCount,
+    OverWeightPatientCount,
+    PcosPatientCount,
+  ] = await Promise.all([
     User.estimatedDocumentCount(),
     Gdm.estimatedDocumentCount(),
+    Infertility.estimatedDocumentCount(),
+    OverWeight.estimatedDocumentCount(),
+    Pcos.estimatedDocumentCount(),
   ]);
 
   const data = {
     userCount,
     GdmPatientCount,
-    totalPatient: GdmPatientCount,
+    InfertilityPatientCount,
+    OverWeightPatientCount,
+    PcosPatientCount,
+    totalPatient:
+      GdmPatientCount +
+      InfertilityPatientCount +
+      OverWeightPatientCount +
+      PcosPatientCount,
   };
 
   return data;
